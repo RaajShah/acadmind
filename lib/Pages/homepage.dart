72f74d8046bccdd314529a897dd1221f0e23c630
raajshah <raajshah100@gmail.com>
2022-04-06 10:57:29 +0530

    lacture74

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,23 +1,46 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    List<Transaction> transaction = [
+      Transaction(
+        id: 't1',
+        title: 'New Shoes',
+        amount: 69.99,
+        date: DateTime.now(),
+      ),
+      Transaction(
+        id: 't2',
+        title: 'Weekly Groceries',
+        amount: 16.53,
+        date: DateTime.now(),
+      ),
+    ];
+
+//________________________________________________
     return Scaffold(
       appBar: AppBar(
         title: const Text('Transation'),
+        backgroundColor: Colors.red,
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
             width: double.infinity,
             child: const Card(
-              child: Text('CHART for Me and Me'),
+              color: Colors.blue,
+              child: Text('CHART'),
+              elevation: 5,
             ),
           ),
           const Card(
+            color: Colors.red,
             child: Text('List of TX'),
           ),
         ],
